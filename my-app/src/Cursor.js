5d6b22e0cd1af617c0306f73acc612abbbe4e7f3
Sheng <shengdatan@gmail.com>
2023-02-01 22:55:54 +0800

    added custom cursor

---
+++ my-app/src/Cursor.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+export default function Cursor() {
+    // small dot cursor
+    const [clientX, setClientX] = useState(-100);
+    const [clientY, setClientY] = useState(-100);
+    
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+        setClientX(event.clientX);
+        setClientY(event.clientY);
+        };
+        document.addEventListener("mousemove", handleMouseMove);
+        return () => {
+        document.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
+    
+    useEffect(() => {
+        const render = () => {
+        requestAnimationFrame(render);
+        };
+        requestAnimationFrame(render);
+    }, []);
+
+    return(
+        <div className="cursor cursor-small" style={{transform: `translate(${clientX}px, ${clientY}px)`}}></div>
+    )
+}
