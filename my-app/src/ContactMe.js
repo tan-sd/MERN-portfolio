8fa0fd0fa427542d39114bc349344e61a705e189
Sheng <shengdatan@gmail.com>
2023-02-01 22:56:24 +0800

    added styling and animation

--- my-app/src/ContactMe.js
+++ my-app/src/ContactMe.js
@@ -7,7 +7,7 @@ export default function ContactMe() {
                 <div className="col-6">
                     <img src="faces/face_contactme.png" className="face-contactme"></img>
                 </div>
-                <div className="pt-5 col-6 myWorks-text rounded-5">
+                <div className="pt-5 col-6 contactMe-text rounded-5">
                     <div className="contactMe-title">CONTACT ME</div>
                 </div>
             </div>
